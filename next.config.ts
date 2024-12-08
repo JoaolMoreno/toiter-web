e38fcaffd5826aa9b567b214d53434f62dbd079a
João Pedro Lopes <jl.moreno7@hotmail.com>
2024-12-08 18:29:26 -0400

    Removendo roteamento root

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/feed',
-                permanent: true,
-            },
-        ];
-    },
+
 };
 
 export default nextConfig;
