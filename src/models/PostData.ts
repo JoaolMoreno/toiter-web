4951055bd0738288f8e8c787d552af03a85e2d3a
João Pedro Lopes <jl.moreno7@hotmail.com>
2024-12-08 18:28:48 -0400

    Removendo propriedades não existentes, e adicionando isLiked

--- src/models/PostData.ts
+++ src/models/PostData.ts
@@ -2,8 +2,6 @@ export interface PostData {
     id: number;
     parentPostId?: number | null;
     repostParentId?: number | null;
-    isRepost: boolean;
-    isReply: boolean;
     username: string;
     content: string;
     mediaUrl?: string | null;
@@ -11,7 +9,8 @@ export interface PostData {
     repliesCount: number;
     repostsCount: number;
     viewCount: number;
-    repostPostData?: PostData | null; // Para aninhamento em reposts
-    deleted?: boolean; // Apenas para controle interno, se necessário
-    createdAt: string; // Data no formato ISO-8601
+    isLiked: boolean;
+    repostPostData?: PostData | null;
+    deleted?: boolean;
+    createdAt: string;
 }
