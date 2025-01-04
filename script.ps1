cadd89777bfea7f27f507f523c6325d478025528
João Pedro Lopes <jl.moreno7@hotmail.com>
2025-01-04 00:07:42 -0400

    pequenos ajutes deploy

--- script.ps1
+++ script.ps1
@@ -1,5 +1,5 @@
 # Build da imagem Docker
-docker build -t lopesz3r4/toiter-front:latest
+docker build -t lopesz3r4/toiter-front:latest .
 
 # Push da imagem para o Docker Hub
 docker push lopesz3r4/toiter-front:latest
